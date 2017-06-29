7ddc16f15582b4911dc092725bb01e81a86474e1
lelouch_0 <lelouch0lampourge@gmail.com>
2017-06-29 23:49:52 +0530

    second method added

--- MATHEMATICAL ALGOS/print all possible permutation of a string.cpp
+++ MATHEMATICAL ALGOS/print all possible permutation of a string.cpp
@@ -3,6 +3,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// METHOD 1 :: USING STL'S next_permutation
 void printPermutation(string s)
 {
 	sort(s.begin(),s.end());
@@ -11,6 +13,23 @@ void printPermutation(string s)
 	{
 		cout<<s<<"\n";
 	}while(next_permutation(s.begin(),s.end()));
+}
+
+// METHOD 2 :: USING RECURSION 
+
+void printPremutation2(string s,int l,int r)
+{
+	if(l==r)
+		cout<<s;
+	else
+	{
+		for(int i=l;i<=r;i++)
+		{
+			swap(s[l],s[i]);
+			printPremutation2(s,l+1,r);
+			swap(s[l],s[i]);
+		}
+	}
 }
 int main()
 {
