666d341bb7e6d28a53a9dcbc3c1baf7b86db77a5
lelouch_0 <lelouch0lampourge@gmail.com>
2017-07-08 22:44:12 +0530

    count ways to reach nth floor

--- MATHEMATICAL ALGOS/count ways to reach nth floor.cpp
+++ MATHEMATICAL ALGOS/count ways to reach nth floor.cpp
@@ -2,7 +2,7 @@
 
 // condition if only allowed steps are 1 and 2
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
@@ -17,13 +17,37 @@ int countWays(int n)
 {
 	return fibo(n+1);
 }
+
+// GENERALIZED ANSWER FOR ANY NO OF STEPS ALLOWED TO REACH Nth FLOOR.
+
+int generalizedAnswer(int n,int m)
+{
+	if(n<=1)return n;
+
+	int res=0;
+
+	for(int i=1; i<=m &&  i<=n; i++)
+	{
+		res+=generalizedAnswer(n-i,m);
+	}
+	return res;
+}
+int countWays2(int n,int m)
+{
+	return generalizedAnswer(n+1,m);
+}
 int main()
 {
 	int n ;
 	cout<<"enter the value of n";
 	cin>>n;
 
-	cout<<countWays(n);
+	cout<<countWays(n)<<endl;
+
+	int m;
+	cout<<"enter the no. of steps allowed\n";
+	cin>>m;
+	cout<<"generalizedAnswer() ::"<<countWays2(n,m);
 }
 
 
